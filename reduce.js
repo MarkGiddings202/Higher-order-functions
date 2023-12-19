abf32662d6948a6e178c5802f3c1407ac875f51a
MarkGiddings202 <gavingiddings1997@gmail.com>
2023-12-18 22:42:29 -0500

    exercise 2

--- reduce.js
+++ reduce.js
@@ -65,3 +65,26 @@ const modded = companies.reduce((result, str, index) => {
 }, "");
 
 console.log(modded);
+
+/*
+    1. Create an array called "prices" with the following values:
+        [1.23, 19.99, 85.2, 32.87, 8, 5.2]
+
+    2. Create a const called "afterTax" that does a sum like so:
+        - If the price is greater than 6, add the price with no tax
+        - Otherwise, add the price plus a tax of 20%
+
+    3. Print out "prices" and "afterTax"
+*/
+
+const prices = [1.23, 19.99, 85.2, 32.87, 8, 5.2];
+
+let afterTax = prices.reduce((result, nums) => {
+  if (nums > 6) {
+    return result + nums;
+  }
+  return result + nums * 1.2;
+}, 0);
+
+console.log(prices);
+console.log(afterTax);
